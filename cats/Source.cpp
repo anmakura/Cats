f3cc4e0dd9d9de17800e05f3d5004e9887d8ef2c
anmakura <andrei.ivanov888twin@gmail.com>
2022-08-11 20:59:53 +0300

    Error to merge

--- cats/Source.cpp
+++ cats/Source.cpp
@@ -5,6 +5,6 @@ int main()
 {
 
 	cout << "Hello Cats!" << endl;
-	cout << "There's nothing to meow, but..." << endl;
-	
+	cout << "There's nothing to meow, but give me a treat!" << endl;
+	cout << "We will calm 'em down now!" << endl;
 }
